b800a5e833bcf837a22779fd61cf5fde90d23132
nickHarperOSU <nick.harper@oregonstate.edu>
2025-01-02 09:50:50 -0800

    removed auth and firebase

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -1,17 +1,12 @@
 <template>
-  <div v-if="user">
+  <div>
     <div class="container">
       <div class="row my-3">
         <div class="col">
           <div
             class="card bg-dark ps-3 py-2 text-light shadow d-flex justify-content-between align-items-center flex-row"
           >
-            <p class="fw-bold my-auto text-start mb-0">
-              Welcome, {{ user.displayName }}
-            </p>
-            <button class="btn btn-outline-light btn-sm me-3" @click="signOut">
-              sign out
-            </button>
+            <p class="fw-bold my-auto text-start mb-0">Welcome, Songwriter</p>
           </div>
         </div>
       </div>
@@ -37,18 +32,13 @@
       </div>
     </div>
   </div>
-  <div v-else>
-    <AuthComponent />
-  </div>
 </template>
 
 <script>
 import PlainTextLayout from "@/components/PlainTextLayout.vue";
 import SongComplete from "@/components/SongComplete.vue";
 import SongLibrary from "@/components/SongLibrary.vue";
-import AuthComponent from "@/components/AuthComponent.vue"; // Updated import
 import { mapGetters, mapActions } from "vuex";
-import { getAuth, signOut as firebaseSignOut } from "firebase/auth"; // Import Firebase auth
 
 export default {
   name: "ActivityView",
@@ -56,19 +46,17 @@ export default {
     SongLibrary,
     SongComplete,
     PlainTextLayout,
-    AuthComponent, // Updated component reference
   },
   computed: {
     ...mapGetters({
       activeSong: "getActiveSong",
-      user: "user",
     }),
   },
   watch: {
     activeSong: {
       handler(newSong) {
         this.localSong = { ...newSong };
-        this.saveStateToFirestore(); // Save state to Firestore immediately
+        this.saveStateToLocalStorage(); // Save state to local storage immediately
         this.activeSongChanged = true; // Set the flag
       },
       immediate: true,
@@ -77,7 +65,7 @@ export default {
       handler(newSong) {
         this.updateSong(newSong);
         if (!this.activeSongChanged) {
-          this.debounceSaveState(); // Debounce state save to Firestore
+          this.debounceSaveState(); // Debounce state save to local storage
         }
       },
       deep: true,
@@ -92,7 +80,7 @@ export default {
     };
   },
   methods: {
-    ...mapActions(["updateSong", "saveStateToFirestore"]),
+    ...mapActions(["updateSong", "saveStateToLocalStorage"]),
     togglePlainText() {
       this.plainTextActive = !this.plainTextActive;
     },
@@ -101,19 +89,9 @@ export default {
       clearTimeout(this.debounceTimeout);
       // Set a new timeout to save state after 1 second of inactivity
       this.debounceTimeout = setTimeout(() => {
-        this.saveStateToFirestore();
+        this.saveStateToLocalStorage();
         this.activeSongChanged = false; // Reset the flag after saving
       }, 1000); // 1000 ms = 1 second
-    },
-    signOut() {
-      const auth = getAuth();
-      firebaseSignOut(auth)
-        .then(() => {
-          console.log("User signed out");
-        })
-        .catch((error) => {
-          console.error("Error signing out: ", error);
-        });
     },
   },
 };
