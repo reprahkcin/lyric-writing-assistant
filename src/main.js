256712695c072427358a34d862c6828185bec005
nickHarperOSU <nick.harper@oregonstate.edu>
2025-01-02 12:26:54 -0800

    rename store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
-import store from "@/store";
+import store from "@/store/store";
 import router from "@/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
