b5231daa11e2d2775df0a8e1578752e95d2b00ec
nickHarperOSU <nick.harper@oregonstate.edu>
2025-01-05 16:54:25 -0800

    firebase removal

--- server/index.js
+++ server/index.js
@@ -1,22 +1,9 @@
 const express = require("express");
-const admin = require("./firebaseAdmin");
 
 const app = express();
 const port = 3000;
 
-app.get("/some-endpoint", async (req, res) => {
-  try {
-    const db = admin.firestore();
-    const doc = await db.collection("lyrics").doc("isE7hppuvNBxN3RHyQDe").get();
-    if (!doc.exists) {
-      res.status(404).send("No such document!");
-    } else {
-      res.status(200).send(doc.data());
-    }
-  } catch (error) {
-    res.status(500).send("Error getting document: " + error);
-  }
-});
+// Define any other necessary endpoints here
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
