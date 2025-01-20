70da40224f803051eaf09d9f5a30978d22d4ba7d
reprahkcin <bignickharper@gmail.com>
2025-01-19 21:50:18 -0800

    Added major minor and chromatic

--- src/components/ScaleMap.vue
+++ src/components/ScaleMap.vue
@@ -5,7 +5,10 @@
   </div>
 </template>
 <script>
-import MajorScales from "@/data/completeScales/majorScales";
+import { generateMajorScaleMap } from "@/data/completeScales/major";
+import { generateMinorScaleMap } from "@/data/completeScales/minor";
+import { ChromaticScaleMap } from "@/data/completeScales/chromatic";
+
 import { SVGuitarChord } from "svguitar";
 export default {
   name: "ScaleComplete",
@@ -13,37 +16,80 @@ export default {
     song: Object,
   },
   data() {
-    return {
-      AMajor: MajorScales.aMajor,
-      BMajor: MajorScales.bMajor,
-      CMajor: MajorScales.cMajor,
-      DMajor: MajorScales.dMajor,
-      EMajor: MajorScales.eMajor,
-      FMajor: MajorScales.fMajor,
-      GMajor: MajorScales.gMajor,
-    };
+    return {};
   },
   methods: {
     selectProperScale() {
-      switch (this.song.key) {
-        case "A":
-          return this.AMajor;
-        case "B":
-          return this.BMajor;
-        case "C":
-          return this.CMajor;
-        case "D":
-          return this.DMajor;
-        case "E":
-          return this.EMajor;
-        case "F":
-          return this.FMajor;
-        case "G":
-          return this.GMajor;
-        default:
-          return this.GMajor;
+      if (this.song.scale === "Major") {
+        return this.selectMajorScale();
+      } else if (this.song.scale === "Minor") {
+        return this.selectMinorScale();
+      } else {
+        return ChromaticScaleMap;
       }
     },
+    selectMajorScale() {
+      switch (this.song.key) {
+        case "A":
+          return generateMajorScaleMap("A");
+        case "A#":
+          return generateMajorScaleMap("A#");
+        case "B":
+          return generateMajorScaleMap("B");
+        case "C":
+          return generateMajorScaleMap("C");
+        case "C#":
+          return generateMajorScaleMap("C#");
+        case "D":
+          return generateMajorScaleMap("D");
+        case "D#":
+          return generateMajorScaleMap("D#");
+        case "E":
+          return generateMajorScaleMap("E");
+        case "F":
+          return generateMajorScaleMap("F");
+        case "F#":
+          return generateMajorScaleMap("F#");
+        case "G":
+          return generateMajorScaleMap("G");
+        case "G#":
+          return generateMajorScaleMap("G#");
+        default:
+          return generateMajorScaleMap("C");
+      }
+    },
+
+    selectMinorScale() {
+      switch (this.song.key) {
+        case "A":
+          return generateMinorScaleMap("A");
+        case "A#":
+          return generateMinorScaleMap("A#");
+        case "B":
+          return generateMinorScaleMap("B");
+        case "C":
+          return generateMinorScaleMap("C");
+        case "C#":
+          return generateMinorScaleMap("C#");
+        case "D":
+          return generateMinorScaleMap("D");
+        case "D#":
+          return generateMinorScaleMap("D#");
+        case "E":
+          return generateMinorScaleMap("E");
+        case "F":
+          return generateMinorScaleMap("F");
+        case "F#":
+          return generateMinorScaleMap("F#");
+        case "G":
+          return generateMinorScaleMap("G");
+        case "G#":
+          return generateMinorScaleMap("G#");
+        default:
+          return generateMajorScaleMap("C");
+      }
+    },
+
     renderScale() {
       const scale = this.selectProperScale();
       try {
@@ -52,6 +98,12 @@ export default {
             frets: 12,
             orientation: "horizontal",
             fretMarkers: [2, 4, 6, 8, { fret: 11, double: true }],
+            fingerColor: "#FFF",
+            fingerTextColor: "#000",
+            fingerTextSize: 18,
+            fingerStrokeColor: "#000",
+            fingerStrokeWidth: "2px",
+            color: "#555",
           })
           .chord(scale)
           .draw();
